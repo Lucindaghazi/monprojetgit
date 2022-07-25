4f65145b53fdb2e4bd7e49a2425d58c6dcbea9fe
LULU <lucinda93@live.fr>
2022-07-25 14:34:52 +0000

    commit de sqaush des 2 précedents commit
     This is good
    
    ok

--- linux_users.sh
+++ linux_users.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 # Script to add a user to Linux system
 if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
+	read -p "reeeEnter username : " username
 	read -s -p "please Enter password : " password
 	egrep "^$username" /etc/passwd >/dev/null
 	if [ $? -eq 0 ]; then
@@ -10,7 +10,8 @@ if [ $(id -u) -eq 0 ]; then
 		exit 1
 	else
 		pass=$(perl -e 'print crypt($ARGV[0], "enter password")' $password)
-		useradd -m -p $pass $username
+		useradd -m -p $pass $usernam
+		e
 		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
 	fi
 else
