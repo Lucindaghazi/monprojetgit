70c83e83f90ba1b20639cf9628034b691b3e559d
LULU <lucinda93@live.fr>
2022-07-25 10:02:34 +0000

    commit linux

--- linux_users.sh
+++ linux_users.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 # Script to add a user to Linux system
 if [ $(id -u) -eq 0 ]; then
@@ -8,7 +9,7 @@ if [ $(id -u) -eq 0 ]; then
 		echo "$username exists!"
 		exit 1
 	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		pass=$(perl -e 'print crypt($ARGV[0], "enter password")' $password)
 		useradd -m -p $pass $username
 		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
 	fi
